eaae291d37b3a90dd1ff28e6491b9d364793a58d
karolisposka <karolisposka96@gmail.com>
2022-12-21 19:13:09 +0200

    mobileMenu - all boards fixed

--- src/components/mobileMenu/MobileMenu.tsx
+++ src/components/mobileMenu/MobileMenu.tsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import * as S from './MobileMenu.styles';
-import {link} from '../../models';
+import { useParams } from 'react-router-dom';
+import { link } from '../../models';
 
 type props = {
-    show: boolean,
-    handleToggle: () => void,
-    theme: string,
-    links: link[],
-}
+  show: boolean;
+  handleToggle: () => void;
+  theme: string;
+  links: link[];
+};
 
-const MobileMenu = ({show, links, handleToggle, theme}:props) => {
+const MobileMenu = ({ show, links, handleToggle, theme }: props) => {
+  const { page } = useParams();
   return (
     <S.Container show={show}>
-        <S.Section>
-            <S.Boards>All boards <S.Span>(2)</S.Span></S.Boards>
-            <S.List>
-              {links ? links.map((link,index) => (
-                      <S.StyledLink key={index} icon={true} to={link.path}>{link.text}</S.StyledLink>
-                  )) : <S.Span style={{padding:'0 16px'}}> No projects yet</S.Span>}
-              </S.List>
-            <S.Toggler handleClick={handleToggle} theme={theme}/>
-        </S.Section>
+      <S.Section>
+        <S.Boards>
+          all boards (<S.Span>{links && links.length}</S.Span>)
+        </S.Boards>
+        <S.List>
+          {links ? (
+            links.map((link, index) => (
+              <S.StyledLink key={index} icon={true} to={link.path}>
+                {link.text}
+              </S.StyledLink>
+            ))
+          ) : (
+            <S.Span style={{ padding: '0 16px' }}> No projects yet</S.Span>
+          )}
+          <S.StyledLink icon={true} to={`/${page}/new`}>
+            Create new board
+          </S.StyledLink>
+        </S.List>
+        <S.Toggler handleClick={handleToggle} theme={theme} />
+      </S.Section>
     </S.Container>
-  )
-}
+  );
+};
 
-export default MobileMenu
+export default MobileMenu;
